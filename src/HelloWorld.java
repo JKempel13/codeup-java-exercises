f5fc8c3c6eed1a73a38d11ef1bcc06bca82acaa2
Jason Kempel <kempel.jason13@gmail.com>
2019-09-16 19:06:35 -0500

    added practice directory to .gitignore

---
+++ src/HelloWorld.java
@@ -0,0 +1,39 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        int myFavoriteNumber = 83;
+        String myString = "Let's go home !";
+//        myString = (char) 'e'; //does not work
+//        System.out.println(myString);
+//        float myFavoriteNumber = 3.14F;
+//        long myNumber;
+//        System.out.println(myNumber);//does not work
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        System.out.println(myFavoriteNumber);
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(y);
+
+        byte nums = -128;
+        nums--;
+//        System.out.println(nums);
+    }
+}
