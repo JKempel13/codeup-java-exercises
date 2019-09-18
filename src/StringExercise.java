de82360cbe2c44b27d3c86bca99cf93506b98943
Jason Kempel <kempel.jason13@gmail.com>
2019-09-18 12:15:31 -0500

    in class exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+//        String message = "We don't need no education \nWe don't need no thought control";
+
+//        String message = "Check \"this\" out, \"s inside of \"s!";
+
+//        String message = "In windows, the main drive is usually C:\\";
+
+        String message = "I can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslash \\\\\\!";
+        System.out.println(message);
+
+    }
+}
