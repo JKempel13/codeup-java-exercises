1f17447c83cd58d8a963a274c58993d64ed406a1
Jason Kempel <kempel.jason13@gmail.com>
2019-09-19 17:09:20 -0500

    exercise 3

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,4 @@
+import jdk.swing.interop.SwingInterOpUtils;
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.Scanner;
@@ -12,7 +13,17 @@ public class MethodsExercises {
 //        System.out.println(divide(7, 4));
 //        System.out.println(findTheRemainder(69, 8));
 
-        getInteger(1,10);
+//        int userNum = getInteger(1,10);
+
+        String yesOrNo = "y";
+        do {
+            factorial(0, sc);
+            System.out.println("Would you like to try another number? Y/N");
+            yesOrNo = sc.nextLine();
+
+        }while (yesOrNo.equalsIgnoreCase("Y"));
+
+
     }
 
     // ==================EXERCISE 1==============================//
@@ -38,16 +49,66 @@ public class MethodsExercises {
 
     // =====================EXERCISE 2==========================//
     public static int getInteger(int min, int max) {
-        System.out.println("Enter a number between 1 and 10: ");
         Scanner sc = new Scanner(System.in);
-        int userInput = sc.nextInt();
-        if (userInput >= min && userInput <=max) {
-            System.out.println("Valid number entered");
-            return userInput;
+        System.out.println("Enter a number between 1 and 10: ");
+        if (sc.hasNextInt()) {
+            int userInput = sc.nextInt();
+            if (userInput >= min && userInput <=max) {
+                return userInput;
+            } else {
+                System.out.println("invalid range");
+            }
         } else {
-            System.out.println("invalid number");
-            return getInteger(min, max);
+          System.out.println("Invalid input");
         }
+        return getInteger(min,max);
+//        int userInput = sc.nextInt();
+//        if (userInput >= min && userInput <=max) {
+//            System.out.println("Valid number entered");
+//            return userInput;
+//        } else {
+//            System.out.println("invalid number");
+//            return getInteger(min, max);
+//        }
 
     }
+
+    // =====================EXERCISE 3======================//
+        public static long factorial(int num, Scanner sc) {
+
+            int input = getInteger(1, 10);
+            long Factorial = 1;
+            for (int i = 1; i <= input; i++) {
+                Factorial *= i;
+            }
+            System.out.printf("\n%d! = %d\n", input, Factorial);
+            return Factorial;
+        }
+    // get valid int
+//    public static int getValidInt() {
+//        Scanner sc = new Scanner(System.in);
+//        while (true) {
+//            if (sc.hasNextInt()) {
+//                return sc.nextInt();
+//            } else {
+//                System.out.println("Invalid input!");
+//                sc.nextLine();
+//            }
+//        }
+//    }
+
+
+    // get valid int in range
+/*    public static int getInt(int min, int max) {
+        while (true) {
+            System.out.println("Please enter a number between " + min + " and " + max);
+            int userNum = getValidInt();
+            if (userNum >= min && userNum <= max) {
+                return userNum;
+            } else {
+                System.out.println("Number out of range!");
+            }
+        }
+    }*/
 }
+
