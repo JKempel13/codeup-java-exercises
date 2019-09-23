f82e969104d32ecb704704e30902ff801ba4935d
Jason Kempel <kempel.jason13@gmail.com>
2019-09-23 14:52:10 -0500

    update exercises

--- src/HighLow.java
+++ src/HighLow.java
@@ -4,13 +4,27 @@ public class HighLow {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        highLow(0);
+        highLow(0, input);
     }
-    public static double highLow (int num){
-        double randomNum = (int) Math.floor(Math.random() * 100 +1);
-        System.out.println(randomNum);
-        System.out.println("Guess the number:");
 
-        return randomNum;
+    public static int highLow(int num, Scanner input) {
+        int randomNum = (int) Math.floor(Math.random() * 100 + 1);
+        boolean found = false;
+
+        while (!found) {
+            System.out.println("Guess the number:");
+            int guessedNumber = input.nextInt();
+
+                if (guessedNumber > randomNum) {
+                    System.out.println("LOWER!");
+                } else if (guessedNumber < randomNum) {
+                    System.out.println("HIGHER!");
+                } else {
+                    System.out.println("GOOD GUESS!");
+                    found = true;
+                }
+        }
+                return num;
     }
 }
+
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -8,22 +8,22 @@ public class MethodsExercises {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-//        System.out.println(add(1, 12));
-//        System.out.println(subtract(83.2, 12.9));
-//        System.out.println(multiply(4, 5));
-//        System.out.println(divide(7, 4));
-//        System.out.println(findTheRemainder(69, 8));
+        /*System.out.println(add(1, 12));
+        System.out.println(subtract(83.2, 12.9));
+        System.out.println(multiply(4, 5));
+        System.out.println(divide(7, 4));
+        System.out.println(findTheRemainder(69, 8));*/
 
-//        int userNum = getInteger(1,10);
+//        int userInputNum = getInteger(1, 10);
 
         //======= part of factorial method==========//
-        /*String yesOrNo = "y";
+       /* String yesOrNo = "y";
         do {
             factorial(0, sc);
             System.out.println("Would you like to try another number? Y/N");
             yesOrNo = sc.nextLine();
 
-        }while (yesOrNo.equalsIgnoreCase("Y"));*/
+        } while (yesOrNo.equalsIgnoreCase("Y"));*/
 
         String yesOrNo = "y";
         do {
@@ -58,33 +58,33 @@ public class MethodsExercises {
     }*/
 
     // =====================EXERCISE 2==========================//
-    /*public static int getInteger(int min, int max) {
+   /* public static int getInteger(int min, int max) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number between 1 and 10: ");
         if (sc.hasNextInt()) {
             int userInput = sc.nextInt();
-            if (userInput >= min && userInput <=max) {
+            if (userInput >= min && userInput <= max) {
                 return userInput;
             } else {
                 System.out.println("invalid range");
             }
         } else {
-          System.out.println("Invalid input");
+            System.out.println("Invalid input");
         }
-        return getInteger(min,max);
+        return getInteger(min, max);
     }*/
 
     // =====================EXERCISE 3======================//
-    /*public static long factorial(int num, Scanner sc) {
+   /* public static long factorial(int num, Scanner sc) {
 
-            int input = getInteger(1, 10);
-            long Factorial = 1;
-            for (int i = 1; i <= input; i++) {
-                Factorial *= i;
-            }
-            System.out.printf("\n%d! = %d\n", input, Factorial);
-            return Factorial;
-        }*/
+        int input = getInteger(1, 10);
+        long Factorial = 1;
+        for (int i = 1; i <= input; i++) {
+            Factorial *= i;
+        }
+        System.out.printf("\n%d! = %d\n", input, Factorial);
+        return Factorial;
+    }*/
 
     // ===================EXERCISE 4=======================//
     public static void dice() {
@@ -96,13 +96,13 @@ public class MethodsExercises {
     public static int diceGame (int min, int max, Scanner sc) {
         Random diceRoll = new Random();
         System.out.println("Are you ready to roll the dice? Y/N");
-        String roll = sc.nextLine();
-        int die1=(int)(Math.random()*6+1);
-        int die2=(int)(Math.random()*6+1);
+        int roll = sc.nextInt();
+        int die1=(int)(Math.random()* roll +1);
+        int die2=(int)(Math.random()* roll +1);
         int sum= die1 + die2;
 
         System.out.printf("You rolled a %s and a %s", die1, die2);
-        return sum;
+        return roll;
     }
 }
 
